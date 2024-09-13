305cdad09f75fb137c7152b4bd35bbb641f4be0a
Guilherme Souza <gui.edu0380@gmail.com>
2024-09-12 21:59:07 -0300

    feat(Header.tsx) adicionando as opções corretas dos móveis

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { Nav, Navbar, NavbarBrand, NavItem } from 'reactstrap';
+import { Nav, Navbar, NavItem } from 'reactstrap';
 import logo from '../app/favicon/icon.jpeg'
 import styles from '../styles/btnHeader.module.css'
 
@@ -8,7 +8,7 @@ export default function Header() {
     return (
         <>
             <Navbar container='md' style={{ backgroundColor: '#d1ae6c' }}>
-                <Nav className="flex-row align-items-center" navbar>
+                <Nav className='flex-row align-items-center' navbar>
                     <Link href='/' className='navbar-brand'>
                         <Image
                             src={logo}
@@ -18,21 +18,41 @@ export default function Header() {
                         />
                     </Link>
 
-                    <NavItem>
-                        <Link href="/products" className={`nav-link ${styles.btnHoverHeader} ${styles.btnStyle}`} passHref>
-                            Produtos
-                        </Link>
-                    </NavItem>
+                    <div className='d-flex'>
+                        <NavItem>
+                            <Link href='/products' className={`nav-link ${styles.btnHoverHeader} ${styles.btnStyle}`} passHref>
+                                Sofás
+                            </Link>
+                        </NavItem>
 
-                    <NavItem>
-                        <Link href="/cart" className={`nav-link ${styles.btnHoverHeader} ${styles.btnStyle}`} passHref>
-                            Carrinho
-                        </Link>
-                    </NavItem>
+                        <NavItem>
+                            <Link href='/cart' className={`nav-link ${styles.btnHoverHeader} ${styles.btnStyle}`} passHref>
+                                Guarda-roupas
+                            </Link>
+                        </NavItem>
+
+                        <NavItem>
+                            <Link href='/cart' className={`nav-link ${styles.btnHoverHeader} ${styles.btnStyle}`} passHref>
+                                Camas
+                            </Link>
+                        </NavItem>
+
+                        <NavItem>
+                            <Link href='/cart' className={`nav-link ${styles.btnHoverHeader} ${styles.btnStyle}`} passHref>
+                                Colchões
+                            </Link>
+                        </NavItem>
+
+                        <NavItem>
+                            <Link href='/cart' className={`nav-link ${styles.btnHoverHeader} ${styles.btnStyle}`} passHref>
+                                Cozinha
+                            </Link>
+                        </NavItem>
+                    </div>
 
                     <div className=''>
                         <NavItem >
-                            <Link href="/cart" className={`nav-link ${styles.btnHoverHeader} ${styles.btnStyle}`} passHref>
+                            <Link href='/cart' className={`nav-link ${styles.btnHoverHeader} ${styles.btnStyle}`} passHref>
                                 Vendas
                             </Link>
                         </NavItem>
