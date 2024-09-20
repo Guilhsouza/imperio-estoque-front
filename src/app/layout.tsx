5c59102cd3f3721e8310076755d4e0e1325c1671
Guilherme Souza <gui.edu0380@gmail.com>
2024-09-19 22:37:12 -0300

    feat(layout.tsx) movimentando Header de Page para Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './globals.css'
 import styles from "./page.module.css";
-import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <>
-      <Header />
       <main className={styles.main}>
         <h2 style={{ marginTop: '5rem' }}>Imperio Sofas</h2>
       </main>
