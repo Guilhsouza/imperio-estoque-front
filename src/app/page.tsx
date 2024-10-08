677b989257acf0907f5e166c97cdd4ca830a5343
Guilherme Souza <gui.edu0380@gmail.com>
2024-10-07 21:54:13 -0300

    feat removendo bootstrap e testando tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './globals.css'
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
     <>
-      <main className={styles.main}>
-        <h2 style={{ marginTop: '5rem' }}>Imperio Sofas</h2>
+      <main>
+        <h2 className="mt-5 text-center">Imperio Sofas</h2>
       </main>
     </>
   );
