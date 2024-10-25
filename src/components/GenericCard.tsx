a4461d4613349ad9f5bc8c1eb6024ad1b5cf0e2e
Guilherme Souza <gui.edu0380@gmail.com>
2024-10-24 22:03:49 -0300

    feat adicionando card genérico para navegar entre páginas no início

--- src/components/GenericCard.tsx
+++ src/components/GenericCard.tsx
@@ -1,16 +1,30 @@
-import { Card, CardHeader, CardBody } from "@nextui-org/card"
-import Image from "next/image"
+interface genericCardProps {
+    backgroundImage: string | StaticImageData,
+    alt: string,
+    href: string
+}
+
+import { Card, CardHeader } from "@nextui-org/card"
+import Image, { StaticImageData } from "next/image"
 import Link from "next/link";
+import React from "react";
 
-export default function GenericCard() {
+const GenericCard: React.FC<genericCardProps> = ({ backgroundImage, alt, href }) => {
     return (
-        <Link href='/sofas'>
-            <Card className="border rounded-lg">
-                <CardBody>
+        <Link href={href}>
+            <Card >
+                <CardHeader className="absolute">
+                    <p>Card genérico</p>
+                </CardHeader>
 
-                    <p>card generico</p>
-                </CardBody>
+                <Image
+                    src={backgroundImage}
+                    alt={alt}
+                    className="bg-cover w-full h-full rounded-lg"
+                />
             </Card>
         </Link>
     );
 }
+
+export default GenericCard
