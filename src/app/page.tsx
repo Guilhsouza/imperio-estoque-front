b8323d3a939a8b46b7071607976268aeb80f6879
Guilherme Souza <gui.edu0380@gmail.com>
2024-11-19 06:12:21 -0300

    adicionando modelo inicial da página home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+import { roboto } from "./fonts/fonts";
+import GenericCardHomePage from "@/components/GenericCardHomePage";
+import sofaImage from "../images/sofa-background.png"
+
 export default function Home() {
   return (
     <>
       <main>
-        <h2 className="mt-5 text-center">Imperio Sofas</h2>
-      </main>
+        <h2 className={`text-center mt-9 mb-5 text-3xl ${roboto.className}`}> Estoque Império Sofás</h2>
+        <div className="w-full max-w-2xl max-h-72 grid grid-cols-2 justify-self-center gap-4">
+          <GenericCardHomePage alt="Imagem do Sofá" href="/sofas" backgroundImage={sofaImage} />
+          <GenericCardHomePage alt="Imagem do Sofá" href="/sofas" backgroundImage={sofaImage} />
+          <GenericCardHomePage alt="Imagem do Sofá" href="/sofas" backgroundImage={sofaImage} />
+          <GenericCardHomePage alt="Imagem do Sofá" href="/sofas" backgroundImage={sofaImage} />
+        </div>
+      </main >
     </>
   );
 }
