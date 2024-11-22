5a83e9801067970c926ea284362fc3eb940a3a3c
Guilherme Souza <gui.edu0380@gmail.com>
2024-11-21 21:42:50 -0300

    feat adicionando metadata title para a página

--- src/app/sofas/page.tsx
+++ src/app/sofas/page.tsx
@@ -1,13 +1,24 @@
+import { Metadata } from "next";
 import { roboto } from "../fonts/fonts";
 import GenericCardSofas from "../../components/GenericCardSofas"
 import sofaEspecified from "../../images/sofa-especified.jpg"
+
+export const metadata: Metadata = {
+    title: 'Sofás | Império Sofás'
+};
 
 export default function SofasPage() {
     return (
         <>
             <h2 className={`text-center text-3xl mt-9 ${roboto.className}`}>Selecione o modelo desejado!</h2 >
-
-            <GenericCardSofas title="Titulo" cardImage={sofaEspecified} altImage="Foto Do Sofá" />
+            <div className="w-full max-h-72 grid grid-cols-4 justify-self-center gap-6 px-16">
+                <GenericCardSofas title="Titulo" cardImage={sofaEspecified} altImage="Foto Do Sofá" />
+                <GenericCardSofas title="Titulo" cardImage={sofaEspecified} altImage="Foto Do Sofá" />
+                <GenericCardSofas title="Titulo" cardImage={sofaEspecified} altImage="Foto Do Sofá" />
+                <GenericCardSofas title="Titulo" cardImage={sofaEspecified} altImage="Foto Do Sofá" />
+                <GenericCardSofas title="Titulo" cardImage={sofaEspecified} altImage="Foto Do Sofá" />
+                <GenericCardSofas title="Titulo" cardImage={sofaEspecified} altImage="Foto Do Sofá" />
+            </div>
         </>
     )
 }
