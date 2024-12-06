72c8564414cba32a42ff1340d16164e1b06192ca
Guilherme Souza <gui.edu0380@gmail.com>
2024-12-05 21:32:28 -0300

    feat alterando card e adicionando a propriedade do link

--- src/app/sofas/page.tsx
+++ src/app/sofas/page.tsx
@@ -12,12 +12,7 @@ export default function SofasPage() {
         <>
             <h2 className={`text-center text-3xl mt-9 ${roboto.className}`}>Selecione o modelo desejado!</h2 >
             <div className="w-full max-h-72 grid grid-cols-4 justify-self-center gap-6 px-16">
-                <GenericCardSofas title="Titulo" cardImage={sofaEspecified} altImage="Foto Do Sofá" />
-                <GenericCardSofas title="Titulo" cardImage={sofaEspecified} altImage="Foto Do Sofá" />
-                <GenericCardSofas title="Titulo" cardImage={sofaEspecified} altImage="Foto Do Sofá" />
-                <GenericCardSofas title="Titulo" cardImage={sofaEspecified} altImage="Foto Do Sofá" />
-                <GenericCardSofas title="Titulo" cardImage={sofaEspecified} altImage="Foto Do Sofá" />
-                <GenericCardSofas title="Titulo" cardImage={sofaEspecified} altImage="Foto Do Sofá" />
+                <GenericCardSofas title="Titulo" cardImage={sofaEspecified} hrefLink='/sofas/imperio' altImage="Foto Do Sofá" />
             </div>
         </>
     )
