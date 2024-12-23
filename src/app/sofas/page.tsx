88aa29c5a93287ab6c5c668f84d8f4d3905ca255
Guilherme Souza <gui.edu0380@gmail.com>
2024-12-23 10:12:24 -0300

    feat adicionando modelo de sofá new york

--- src/app/sofas/page.tsx
+++ src/app/sofas/page.tsx
@@ -12,7 +12,8 @@ export default function SofasPage() {
         <>
             <h2 className={`text-center text-3xl mt-9 ${roboto.className}`}>Selecione o modelo desejado!</h2 >
             <div className="w-full max-h-72 grid grid-cols-4 justify-self-center gap-6 px-16">
-                <GenericCardSofas title="Titulo" cardImage={sofaEspecified} hrefLink='/sofas/imperio' altImage="Foto Do Sofá" />
+                <GenericCardSofas title="Sofa xyz" cardImage={sofaEspecified} hrefLink='/sofas/imperio' altImage="Foto Do Sofá" />
+                <GenericCardSofas title="Sofa xpto" cardImage={sofaEspecified} hrefLink='/sofas/newyork' altImage="Foto Do Sofá" />
             </div>
         </>
     )
