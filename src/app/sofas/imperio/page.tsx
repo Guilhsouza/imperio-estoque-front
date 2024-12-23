1b45d7c8abff41ff31a7af3c9f8735fdc73d2f1d
Guilherme Souza <gui.edu0380@gmail.com>
2024-12-23 10:11:22 -0300

    feat alterando forma de trabalhar com as páginas genéricas para facilitar o desenvolvimento

--- src/app/sofas/imperio/page.tsx
+++ src/app/sofas/imperio/page.tsx
@@ -1,5 +1,7 @@
 import { Metadata } from 'next';
 import GenericPageForSofaModels from '../../../components/GenericPageForSofaModels'
+import SpecifiedSofaCard from "../../../components/SpecifiedSofaCard";
+import sofaEspecified from "../../../images/sofa-especified.jpg"
 
 export const metadata: Metadata = {
     title: 'Sofás | Império Sofás'
@@ -7,6 +9,26 @@ export const metadata: Metadata = {
 
 export default function imperioPage() {
     return (
-        <GenericPageForSofaModels />
+        <GenericPageForSofaModels
+            generalSofaTitle='Titulo do Sofa'
+
+            sofaDescription='Lorem ipsum dolor sit amet consectetur adipisicing elit.Quidem corrupti maiores, possimus ad sint temporibus recusandae ducimus officiis 
+            sequi dolorum assumenda tempore nisi asperiores incidunt ipsum tempora repellat suscipit quo enim! Autem expedita, similique officia incidunt ex odit! 
+            Voluptate temporibus ipsum natus dolorum inventore, illo molestiae minima consequatur facilis.Non illo repudiandae consequatur nostrum cumque amet 
+            perferendis asperiores reiciendis provident quaerat veniam, ex laborum porro corrupti excepturi.Harum quasi consequuntur distinctio qui ex dolores beatae 
+            ullam ? Iure at, quos vero unde maiores rerum.Earum, quas quaerat! Amet quam corporis accusantium similique a maxime perspiciatis provident esse, 
+            tenetur eius id voluptatum.'
+
+            sofaCards={[
+                <>
+                    <SpecifiedSofaCard title='sofa X 2.10' altImage='foto do sofa' cardImage={sofaEspecified} />
+                    <SpecifiedSofaCard title='sofa X 2.10' altImage='foto do sofa' cardImage={sofaEspecified} />
+                    <SpecifiedSofaCard title='sofa X 2.10' altImage='foto do sofa' cardImage={sofaEspecified} />
+                    <SpecifiedSofaCard title='sofa X 2.10' altImage='foto do sofa' cardImage={sofaEspecified} />
+                </>
+            ]}
+        />
+
+
     )
 }
--- src/components/GenericPageForSofaModels.tsx
+++ src/components/GenericPageForSofaModels.tsx
@@ -1,25 +1,19 @@
-import SpecifiedSofaCard from "../components/SpecifiedSofaCard";
-import sofaEspecified from "../images/sofa-especified.jpg"
+import { ReactNode } from "react";
 
-interface genericInterface {
-
+interface SpecifiedSofaProps {
+    generalSofaTitle: string,
+    sofaDescription: string,
+    sofaCards: Array<ReactNode>
 }
 
-const GenericPageForSofaModels = () => {
+const GenericPageForSofaModels: React.FC<SpecifiedSofaProps> = ({ generalSofaTitle, sofaDescription, sofaCards }) => {
     return (
         <>
-            <h2 className="mt-9 text-3xl text-center">Titulo do sofá</h2>
-            <p className="mt-5 mx-36 text-gray-400 text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quo atque ipsam nisi, itaque id voluptates veniam accusamus
-                illum, reiciendis facilis aut adipisci quasi impedit optio cumque cum earum, commodi quod maxime quidem blanditiis. Optio at aperiam obcaecati ducimus
-                sunt fugiat doloremque quo debitis explicabo nihil! Nemo provident cum vero quasi quod aliquam eaque iusto perspiciatis quo, tenetur officia omnis
-                perferendis error animi repudiandae aliquid consectetur suscipit at nisi numquam necessitatibus minima? Quos, natus nostrum! Vitae ex quo esse nihil
-                atque, placeat odit sint ipsam et sequi porro dolor dolores eum aliquam aliquid iure ipsa amet nulla laboriosam suscipit. Laboriosam.</p>
+            <h2 className="mt-9 text-3xl text-center">{generalSofaTitle}</h2>
+            <p className="mt-5 mx-36 text-gray-400 text-center">{sofaDescription}</p>
 
             <div className="w-full max-h-72 grid grid-cols-4 justify-self-center gap-6 mt-6 px-16">
-                <SpecifiedSofaCard title={"Sofá X 2.10m"} cardImage={sofaEspecified} altImage={""} />
-                <SpecifiedSofaCard title={"Sofá X 2.10m"} cardImage={sofaEspecified} altImage={""} />
-                <SpecifiedSofaCard title={"Sofá X 2.10m"} cardImage={sofaEspecified} altImage={""} />
-                <SpecifiedSofaCard title={"Sofá X 2.10m"} cardImage={sofaEspecified} altImage={""} />
+                {sofaCards}
             </div>
         </>
     )
