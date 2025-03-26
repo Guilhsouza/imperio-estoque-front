600a39bcaab1cba3a4e19291090b6a1e9becab0f
Guilherme Souza <gui.edu0380@gmail.com>
2025-03-26 20:00:26 -0300

    feat finalizando escopo da página de camas e colchoes

--- src/app/bedsAndMattress/page.tsx
+++ src/app/bedsAndMattress/page.tsx
@@ -1,35 +1,50 @@
-import { Metadata } from "next";
+'use client'
 import { roboto } from "../fonts/fonts";
-import SpecifiedSofaCard from "@/components/sofaPageComponents/SpecifiedSofaCard";
+import SpecifiedBedsCard from "@/components/bedsPageComponents/SpecifiedBedsCard";
 import camaGeneric from "@/images/cama-generic.jpg"
-
-export const metadata: Metadata = {
-    title: "Camas e Sofás | Império Sofás"
-};
-
+import { useRef } from "react";
 
 export default function bedsAndMattress() {
+    const mattressElement = useRef<HTMLDivElement | null>(null)
+
+    const scrollToMattressSection = () => {
+        mattressElement.current?.scrollIntoView({ behavior: 'smooth' })
+    }
+
+
     return (
         <>
+            <title>Camas e Colchões | Império Sofás</title>
             <h2 className={`text-center text-3xl mt-9 ${roboto.className}`}>Camas e Colchões</h2>
             <p className="text-center mt-5 text-gray-400">Aqui você vai encontrar o estoque das camas e colchões!</p>
 
             <div className="flex justify-center">
-                <button className="rounded-full mt-5 p-2 bg-standardGreen text-standardBlack">Quer pular para os colchões? Clique Aqui!</button>
+                <button
+                    className="rounded-full mt-5 p-2 bg-standardGreen text-standardBlack"
+                    onClick={scrollToMattressSection}>
+                    Quer pular para os colchões? Clique Aqui!
+                </button>
             </div>
 
             <div id="beds">
                 <h3 className="underline text-4xl ms-16">Camas</h3>
-                <div className=" grid grid-cols-4 gap-6 justify-self-center px-16">
-                    <SpecifiedSofaCard altImage="cama padrao" title="Cama yxz" cardImage={camaGeneric} />
-                    <SpecifiedSofaCard altImage="cama padrao" title="Cama yxz" cardImage={camaGeneric} />
-                    <SpecifiedSofaCard altImage="cama padrao" title="Cama yxz" cardImage={camaGeneric} />
-                    <SpecifiedSofaCard altImage="cama padrao" title="Cama yxz" cardImage={camaGeneric} />
+                <div className="w-full grid grid-cols-4 justify-self-center gap-6 px-16">
+                    <SpecifiedBedsCard altImage="cama padrao" title="Cama yxz" cardImage={camaGeneric} />
+                    <SpecifiedBedsCard altImage="cama padrao" title="Cama yxz" cardImage={camaGeneric} />
+                    <SpecifiedBedsCard altImage="cama padrao" title="Cama yxz" cardImage={camaGeneric} />
+                    <SpecifiedBedsCard altImage="cama padrao" title="Cama yxz" cardImage={camaGeneric} />
                 </div>
             </div>
             <hr className="mx-16 my-12" />
-            <div id="mattress">
+            <div id="mattress" ref={mattressElement}>
                 <h3 className="underline text-4xl ms-16">Colchões</h3>
+
+                <div className="w-full grid grid-cols-4 justify-self-center gap-6 px-16">
+                    <SpecifiedBedsCard altImage="cama padrao" title="Cama yxz" cardImage={camaGeneric} />
+                    <SpecifiedBedsCard altImage="cama padrao" title="Cama yxz" cardImage={camaGeneric} />
+                    <SpecifiedBedsCard altImage="cama padrao" title="Cama yxz" cardImage={camaGeneric} />
+                    <SpecifiedBedsCard altImage="cama padrao" title="Cama yxz" cardImage={camaGeneric} />
+                </div>
             </div>
         </>
     )
---
+++ src/components/bedsPageComponents/SpecifiedBedsCard.tsx
@@ -0,0 +1,42 @@
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
+import Image, { StaticImageData } from "next/image";
+import styles from "@/styles/editBtnHover.module.css"
+import stylesFromImg from '@/styles/standardizeSizeImg.module.css'
+
+interface typesForBedsCard {
+    title: string;
+    cardImage: string | StaticImageData
+    altImage: string
+}
+
+const SpecifiedBedsCard: React.FC<typesForBedsCard> = ({ title, cardImage, altImage }) => {
+    const stockResult = 399
+
+    return (
+        <Card shadow="sm" className="bg-neutral-800 rounded-lg mt-8">
+            <CardHeader className="pb-0 flex-col items-center">
+                <h3>{title}</h3>
+            </CardHeader>
+
+            <CardBody>
+                <div className={`${stylesFromImg.standardizeSizeImg}`}>
+                    <Image
+                        src={cardImage}
+                        alt={altImage}
+                        fill
+                        className="rounded-lg object-cover"
+                    />
+                </div>
+            </CardBody>
+
+            <CardFooter>
+                <div className="flex justify-around w-full">
+                    <p>Quantidade no Estoque: {stockResult}</p>
+                    <button className={`border-solid border-2 rounded-full px-4 ${styles.btnHoverDefaultColor} ${styles.btnHover}`}>editar</button>
+                </div>
+            </CardFooter>
+        </Card >
+    )
+}
+
+export default SpecifiedBedsCard
