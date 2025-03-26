1f06c689a50ed6aec8d43f7642da04f037a2f086
Guilherme Souza <gui.edu0380@gmail.com>
2025-03-26 20:01:14 -0300

    retirando cursor-pointer da imagem

--- src/app/sofas/imperio/page.tsx
+++ src/app/sofas/imperio/page.tsx
@@ -16,9 +16,7 @@ export default function imperioPage() {
 
             sofaDescription='Lorem ipsum dolor sit amet consectetur adipisicing elit.Quidem corrupti maiores, possimus ad sint temporibus recusandae ducimus officiis 
             sequi dolorum assumenda tempore nisi asperiores incidunt ipsum tempora repellat suscipit quo enim! Autem expedita, similique officia incidunt ex odit! 
-            Voluptate temporibus ipsum natus dolorum inventore, illo molestiae minima consequatur facilis.Non illo repudiandae consequatur nostrum cumque amet 
-            perferendis asperiores reiciendis provident quaerat veniam, ex laborum porro corrupti excepturi.Harum quasi consequuntur distinctio qui ex dolores beatae 
-            ullam ? Iure at, quos vero unde maiores rerum.Earum, quas quaerat! Amet quam corporis accusantium similique a maxime perspiciatis provident esse, 
+            Voluptate temporibus maiores rerum.Earum, quas quaerat! Amet quam corporis accusantium similique a maxime perspiciatis provident esse, 
             tenetur eius id voluptatum.'
 
             sofaCards={[
