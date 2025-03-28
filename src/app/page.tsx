cdbf8fe154bd0adc6fbb30f88c0202791f4b3ea6
Guilherme Souza <gui.edu0380@gmail.com>
2025-03-27 21:41:35 -0300

    feat alterando direção do link dos botões

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,9 @@ export default function Home() {
         <h2 className={`text-center my-10 text-3xl ${roboto.className}`}> Estoque Império Sofás</h2>
         <div className="max-w-5xl grid grid-cols-2 justify-self-center gap-x-6 gap-y-12 w-full  h-full">
           <GenericCardHomePage textFooter="Sofás" alt="Imagem do Sofá" href="/sofas" backgroundImage={sofaImage} />
-          <GenericCardHomePage textFooter="Camas e Colchões" alt="Imagem do Sofá" href="/sofas" backgroundImage={sofaImage} />
-          <GenericCardHomePage textFooter="Sala" alt="Imagem do Sofá" href="/sofas" backgroundImage={sofaImage} />
-          <GenericCardHomePage textFooter="Cozinha" alt="Imagem do Sofá" href="/sofas" backgroundImage={sofaImage} />
+          <GenericCardHomePage textFooter="Camas e Colchões" alt="Imagem do Sofá" href="/bedsAndMattress" backgroundImage={sofaImage} />
+          <GenericCardHomePage textFooter="Sala" alt="Imagem do Sofá" href="/room" backgroundImage={sofaImage} />
+          <GenericCardHomePage textFooter="Cozinha" alt="Imagem do Sofá" href="/kitchen" backgroundImage={sofaImage} />
         </div>
       </main >
     </>
