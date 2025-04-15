cf094275ad117c2b9226b0d1b586d18ce2fcb1c5
Guilherme Souza <gui.edu0380@gmail.com>
2025-04-14 21:58:10 -0300

    adicionando mais card para teste

--- src/app/sofas/page.tsx
+++ src/app/sofas/page.tsx
@@ -11,8 +11,14 @@ export default function SofasPage() {
     return (
         <>
             <h2 className={`text-center text-3xl mt-9 ${roboto.className}`}>Selecione o modelo desejado!</h2 >
-            <div className="w-full max-h-72 grid grid-cols-4 justify-self-center gap-6 px-16">
+            <div className="w-full grid grid-cols-4 justify-self-center gap-6 px-16">
                 <GenericCardSofas title="Sofa xyz" cardImage={sofaEspecified} hrefLink='/sofas/imperio' altImage="Foto Do Sofá" />
+                <GenericCardSofas title="Sofa xpto" cardImage={sofaEspecified} hrefLink='/sofas/newyork' altImage="Foto Do Sofá" />
+                <GenericCardSofas title="Sofa xpto" cardImage={sofaEspecified} hrefLink='/sofas/newyork' altImage="Foto Do Sofá" />
+                <GenericCardSofas title="Sofa xpto" cardImage={sofaEspecified} hrefLink='/sofas/newyork' altImage="Foto Do Sofá" />
+                <GenericCardSofas title="Sofa xpto" cardImage={sofaEspecified} hrefLink='/sofas/newyork' altImage="Foto Do Sofá" />
+                <GenericCardSofas title="Sofa xpto" cardImage={sofaEspecified} hrefLink='/sofas/newyork' altImage="Foto Do Sofá" />
+                <GenericCardSofas title="Sofa xpto" cardImage={sofaEspecified} hrefLink='/sofas/newyork' altImage="Foto Do Sofá" />
                 <GenericCardSofas title="Sofa xpto" cardImage={sofaEspecified} hrefLink='/sofas/newyork' altImage="Foto Do Sofá" />
             </div>
         </>
